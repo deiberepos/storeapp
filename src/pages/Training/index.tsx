f385fb735ef7a809c88c748c21bcc7f84568d8c0
Deiber Gaviria <deiberalex@gmail.com>
2025-10-29 16:01:55 -0500

    feat: implementación del panel de entrenamientos
    - Añadida sección de entrenamientos con diseño responsivo
    - Implementados filtros por nivel y búsqueda
    - Diseño de tarjetas con íconos y colores según el mockup
    - Añadida funcionalidad de 'Elegir entrenador'

--- src/pages/Training/index.tsx
+++ src/pages/Training/index.tsx
@@ -1,18 +1,17 @@
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { useTranslation } from 'react-i18next'
+import TrainingList from '../../components/training/TrainingList'
 
 export default function Training() {
   const { t } = useTranslation()
   
   return (
-    <Box sx={{ textAlign: 'left' }}>
-      <Typography variant="h4" component="h1" gutterBottom>
+    <Box>
+      <Typography variant="h4" component="h1" gutterBottom sx={{ mb: 4 }}>
         {t('navigation.training')}
       </Typography>
-      <Typography variant="body1">
-        Sección en desarrollo...
-      </Typography>
+      <TrainingList />
     </Box>
   )
 }
